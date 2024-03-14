4692332511b15992626ae171dcfc2c071981276f
Paulo Ricardo de Souza Silva <30639809+pauloricardosouza@users.noreply.github.com>
2024-03-14 15:23:18 -0300

    Add files via upload
    Commit realizado com alterações aplicadas na aula do dia 14 de março.

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php include("header.php") ?>
+
+        <h1>Aqui ficará o conteúdo da página</h1>
+        <h1>Aqui ficará o conteúdo da página</h1>
+        <h1>Aqui ficará o conteúdo da página</h1>
+        <h1>Aqui ficará o conteúdo da página</h1>
+        <h1>Aqui ficará o conteúdo da página</h1>
+        <h1>Aqui ficará o conteúdo da página</h1>
+        <h1>Aqui ficará o conteúdo da página</h1>
+        <h1>Aqui ficará o conteúdo da página</h1>
+        <h1>Aqui ficará o conteúdo da página</h1>
+        <h1>Aqui ficará o conteúdo da página</h1>
+        <h1>Aqui ficará o conteúdo da página</h1>
+        <h1>Aqui ficará o conteúdo da página</h1>
+        <h1>Aqui ficará o conteúdo da página</h1>
+        <h1>Aqui ficará o conteúdo da página</h1>
+        <h1>Aqui ficará o conteúdo da página</h1>
+        <h1>Aqui ficará o conteúdo da página</h1>
+        <h1>Aqui ficará o conteúdo da página</h1>
+        <h1>Aqui ficará o conteúdo da página</h1>
+        <h1>Aqui ficará o conteúdo da página</h1>
+        <h1>Aqui ficará o conteúdo da página</h1>
+        <h1>Aqui ficará o conteúdo da página</h1>
+        <h1>Aqui ficará o conteúdo da página</h1>
+    
+<?php include("footer.php") ?>
